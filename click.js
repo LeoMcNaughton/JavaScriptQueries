53a7779e94a5e1d1e7a811672aa725f2a7b74737
Leonardo McNaughton <9lmcnaughton@stpatrick.org>
2017-03-23 10:49:19 -0500

    A solid day 3!

--- click.js
+++ click.js
@@ -1,6 +1,14 @@
 //declare samecolor() here
 function samecolor(){
-    let color
+    const doc = documet;
+    let color = "#009900";
+
+    let divs = doc.querySelectorAll("#samecolor div")
+
+    for(var i = 0; i < array.length; i++;)
+    divs[0].style.backgroundcolor = color;
+    divs[0].height
+
 }
 //declare diffcolor() here
 function diffcolor(){
